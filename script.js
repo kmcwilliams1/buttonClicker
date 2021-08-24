028d5d8cac93fd785109dc6bb7afbaf21a1f72da
Kyle McWilliams <kmac52694@gmail.com>
2021-08-23 21:39:14 -0500

    timer working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // let namee = localStorage.getItem("name") 
 //     var list = document.createElement("li")
 //     list.innerText = namee
@@ -6,55 +8,94 @@
 
 
 
-// //timer
+
+
+      
+      
+      // let numbers = [1,2,3,4,5,6]
+      // while (numbers.length > 0 ){
+         //    var randomNumber = Math.floor(Math.random()* numbers.length)
+         //    console.log(numbers[randomNumber])
+         //    numbers.splice(randomNumber , 1)
+         // }
+         
+         // //buttons and getting them to disappear on click
+         // const oneBtn = document.getElementById('1')
+         // const twoBtn = document.getElementById('2')
+         // const threeBtn = document.getElementById('3')
+         // const fourBtn = document.getElementById('4')
+         // const fiveBtn = document.getElementById('5')
+         // const sixBtn = document.getElementById('6')
+         // const clickMeEl = document.getElementsByClassName('clickMe')
+         
+         // clickMeEl.addEventListener('click' , function(){
+            //     this.id.ClassList.add('hide')  //how to get this to make buttons disappear on click?
+            // })
+            
+            
+            
+            // //starting the game    why are the buttons showing and why isnt the timer going?
+            const startEl = document.getElementById('start')
+
+
+            startEl.addEventListener('click' , startGame)
+
+            function startGame (e){
+               timerId = setInterval(timer, 1000)
+               timerEl.textContent = timeLeft;
+               e.preventDefault()
+
+               startEl.classList.add('hide')
+      //          let numbers = [1,2,3,4,5,6]
+      //          while (numbers.length > 0 ){
+      //  var randomNumber = Math.floor(Math.random()* numbers.length)
+      //  console.log(numbers[randomNumber])
+      //  numbers.splice(randomNumber , 1)
+   //  }
+   //  for (let i = 0; i < randomNumber.length; i++) {
+   //    var button = document.createElement("button");
+   //    button.innerHTML = randomNumber[i];
+   //    var buttonDiv = document.getElementById("buttons");
+   //    buttonDiv.appendChild(button);
+    }
+
+//}
+
+
+
+//timer   all working!
 // const timerEl = document.getElementById('timer');
 // var timeLeft = 10;
-// var timerId; 
+// var timerId;
+
+
 // function timer () {
-//     timeLeft--;
-//     timerEl.textContent = timeLeft;
-//     if(timeLeft <=0){
-//     endGame();
+//    timeLeft--;
+//    timerEl.textContent = timeLeft;
+//    if(timeLeft <=0){
+//    endQuiz();
+// }
 // }
 
 
-// let numbers = [1,2,3,4,5,6]
-// while (numbers.length > 0 ){
-//    var randomNumber = Math.floor(Math.random()* numbers.length)
-//    console.log(numbers[randomNumber])
-//    numbers.splice(randomNumber , 1)
+
+
+
+// //ending the game
+
+// const scoresEl = document.getElementById('scores)
+// function endGame(){
+//     scoreEl.ClassList.remove('hide')
+//     buttonsEl.classList.add('hide')
+
 // }
-
-// //buttons and getting them to disappear on click
-// const oneBtn = document.getElementById('1')
-// const twoBtn = document.getElementById('2')
-// const threeBtn = document.getElementById('3')
-// const fourBtn = document.getElementById('4')
-// const fiveBtn = document.getElementById('5')
-// const sixBtn = document.getElementById('6')
-// const clickMeEl = document.getElementsByClassName('clickMe')
-
-// clickMeEl.addEventListener('click' , function(){
-//     this.id.ClassList.add('hide')  //how to get this to make buttons disappear on click?
-// })
-
-
-
-// //starting the game    why are the buttons showing and why isnt the timer going?
-const startEl = document.getElementById('start')
-startEl.addEventListener('click' , startGame)
-function startGame (){
-    timerId = setInterval(timer, 1000)
-    timerEl.textContent = timeLeft;
-    startEl.classList.add('hide')
-   
-}
 
 
 
 
 
 //for displaying and saving high scores
+
 // const clickEl = document.querySelector('.click')
 // clickEl.addEventListener('click' , submitName) 
 // function submitName(){
@@ -67,15 +108,4 @@ function startGame (){
 // }
 
 
-
-
-// //ending the game
-
-// const scoresEl = document.getElementById('scores)
-
-// function endGame(){
-//     scoreEl.ClassList.remove('hide')
-//     buttonsEl.classList.add('hide')
-
-// }
 
