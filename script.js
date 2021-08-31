db49fb8bf97fbc49fad77a8df425ef4536306676
Kyle McWilliams <kmac52694@gmail.com>
2021-08-31 10:22:51 -0500

    adding id to buttons

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 
 // this has to stay at the top. it is for getting the high scores names and appending 
 // them to the page. do not move it, ya dingus!
+
 // let namee = localStorage.getItem("name") 
 //     var list = document.createElement("li")
 //     list.innerText = namee
 // const appendHereEl = document.getElementById('append-here') 
 //  appendHereEl.append(list)
+let buttonDiv = document.querySelector(".buttons");
+
+let alreadyClicked = []
+buttonDiv.addEventListener('click', function hasItBeenClicked(event) {
+  if (event.target.matches("button")) {
+    let clickedNumber = parseInt(event.target.id) 
+    console.log(clickedNumber)
+      alreadyClicked.push(clickedNumber)   
+    for (let i = 0; i < clickedNumber; i++) {
+      const currentNumber = i;
+       console.log(currentNumber)
+    }
+  }
+})
 
 
+// //starting the game   
+const startEl = document.getElementById('start')
+startEl.addEventListener('click', startGame)
+function startGame(event) {
+  // timerId = setInterval(timer, 1000)
+  // timerEl.textContent = timeLeft;
+  event.preventDefault()
+  startEl.classList.add('hide')
+  let numbers = [1, 2, 3, 4, 5, 6]
+  while (numbers.length > 0) {
+    let randomNumber = Math.floor(Math.random() * numbers.length)
+    console.log(numbers[randomNumber])
+    let button = document.createElement("button");
 
-
-            
-            // //starting the game   
-            const startEl = document.getElementById('start')
-            startEl.addEventListener('click' , startGame)
-            function startGame (e){
-               // timerId = setInterval(timer, 1000)
-               // timerEl.textContent = timeLeft;
-               e.preventDefault()
-               startEl.classList.add('hide')
-               let numbers = [1,2,3,4,5,6]
-               while (numbers.length > 0 ){
-       var randomNumber = Math.floor(Math.random()* numbers.length)
-       console.log(numbers[randomNumber])
-       numbers.splice(randomNumber , 1)
-
-      var button = document.createElement("button");
-     
-      var buttonDiv = document.querySelector(".buttons");
-      button.innerHTML = numbers[randomNumber];
-      buttonDiv.appendChild(button);
-       
-   }
-  
+    button.setAttribute("id", numbers[randomNumber])
+    button.innerHTML = numbers[randomNumber];
+    buttonDiv.appendChild(button);
+    numbers.splice(randomNumber, 1)
+  }
 
 
 }
