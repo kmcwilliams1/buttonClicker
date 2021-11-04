55cc887bc4d61980e7bc1e42a10dbae9158748b2
Kyle McWilliams <kmac52694@gmail.com>
2021-11-04 14:55:14 -0500

    cant get hide working

--- script.js
+++ script.js
@@ -20,56 +20,48 @@ startEl.addEventListener('click', startGame)
 function startGame(event) {
   // timerId = setInterval(timer, 1000)
   // timerEl.textContent = timeLeft;
-  game()
+  createButtons()
   event.preventDefault()
   startEl.classList.add('hide')
 }
 
-//how to say if all buttons are clicked, refresh them?
+//how to say if all buttons are clicked, refresh them
 // if(hasBeenClicked.length === 5){
-//   game()
+//   createButtons()
 // }
 
-const game = () => {
-  let numbers = [1, 2, 3, 4, 5, 6]
-  let hasBeenClicked = []  
 
-  function createButtons () {
-    while (numbers.length > 0) {
-      let button = document.createElement("button");
-      let randomNumber = Math.floor(Math.random() * numbers.length)
-      console.log(numbers[randomNumber])
-      button.setAttribute("id", numbers[randomNumber])
-      button.innerHTML = numbers[randomNumber];
-      buttonDiv.appendChild(button);
-      numbers.splice(randomNumber, 1)
-    }
+let numbers = [1, 2, 3, 4, 5, 6]
+let hasBeenClicked = []
+
+function createButtons() {
+  while (numbers.length > 0) {
+    let button = document.createElement("button");
+    let randomNumber = Math.floor(Math.random() * numbers.length)
+    console.log(numbers[randomNumber])
+    button.setAttribute("id", numbers[randomNumber])
+    button.innerHTML = numbers[randomNumber];
+    buttonDiv.appendChild(button);
+    numbers.splice(randomNumber, 1)
   }
-
-  buttonDiv.addEventListener('click', function(event) {
-      let clickedNumber = parseInt(event.target.id)
-      console.log(clickedNumber)
-      let alreadyClicked = false
-      for (let i = 1; i < 6; i++) {
-        const currentNumber = i;
-        hasBeenClicked.includes(currentNumber)
-        if (hasBeenClicked.length === 0 && clickedNumber === 1) {
-          console.log("yes!")
-          alreadyClicked === true
-          button.addClass('hide')
-          hasBeenClicked.push(clickedNumber)
-        }
-       }
-  }
-  
-  )
- 
-
-  createButtons()
 }
 
-
-
+buttonDiv.addEventListener('click', function  (event) {
+  let clickedNumber = parseInt(event.target.id)
+  console.log(clickedNumber)
+  let alreadyClicked = false
+  for (let i = 1; i < 6; i++) {
+    const currentNumber = i;
+    hasBeenClicked.includes(currentNumber)
+    if (hasBeenClicked.length === 0 && clickedNumber === 1) {
+      console.log("yes!")
+      alreadyClicked === true
+      let word = event.target.id
+      word.addClass('hide')
+      hasBeenClicked.push(clickedNumber)
+    }
+  }
+})
 
 
 
